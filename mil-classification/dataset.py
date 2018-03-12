08cf50a3f1dbe35ac871db1cd131e13052f13e15
Ole Ostergaard <ole.ostergaard@knime.com>
2018-03-12 14:20:21 +0100

    wip start dataset api integration

--- mil-classification/dataset.py
+++ mil-classification/dataset.py
@@ -1,2 +1,16 @@
+#from tensorflow.contrib.data import Dataset, Iterator
 import tensorflow as tf
-from
+import data
+
+def input_parser(img_path):
+    label_encoder = data.labelencoder()
+    label =  label_encoder.transform([data.getlabel(img_path)])
+    one_hot = tf.one_hot(label, 6)
+
+    img_file = tf.read_file(img_path)
+    img_decoded = tf.image.decode_image(img_file, channels=3)
+
+    return img_decoded, one_hot
+
+
+img_decoded, one_hot = input_parser("GBM")
