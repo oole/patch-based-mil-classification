22cce8825fa1c5713ec51962fad27e3e1cf3de90
Ole Ostergaard <ole.ostergaard@knime.com>
2018-04-05 10:37:49 +0200

    add logreg model trainer

---
+++ mil-classification/train_logreg.py
@@ -0,0 +1,52 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.externals import joblib
+import data
+from sklearn.utils import shuffle
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+
+
+
+
+def train_logreg_from_histograms_and_labels(histograms, labels, savepath=None):
+    model = LogisticRegression(verbose=1)
+    model.fit(histograms, labels)
+    print(model)
+    print("train_logreg, finished")
+    if savepath is not None:
+        joblib.dump(model, savepath)
+        print("train_logreg, saved model to:  " + savepath)
+    return model
+
+def save_logreg_model(model, savepath):
+    joblib.dump(model, savepath)
+    print("train_logreg, saved model to:  " + savepath)
+
+def test_logreg(datapath, loadpath):
+    histograms, labels = data.read_histograms_and_labels_from_file(datapath)
+    model = joblib.load(loadpath)
+    predictions = model.predict(histograms)
+    accuracy = accuracy_score(labels, predictions)
+    print (accuracy)
+    confusion = confusion_matrix(labels, predictions)
+    print(confusion)
+
+
+def test_given_logreg(histograms, labels, logreg_model):
+    predictions = logreg_model.predict(histograms)
+    accuracy = accuracy_score(labels, predictions)
+    print(accuracy)
+    confusion = confusion_matrix(labels, predictions)
+    print(confusion)
+    return accuracy, confusion
+
+def test_logreg2(histograms, labels, loadpath):
+    model = joblib.load(loadpath)
+    predictions = model.predict(histograms)
+    accuracy = accuracy_score(labels, predictions)
+    print(accuracy)
+    confusion = confusion_matrix(labels, predictions)
+    print(confusion)
+    return accuracy, confusion
+
+
