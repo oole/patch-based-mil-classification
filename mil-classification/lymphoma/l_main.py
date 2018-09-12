0fb7fb722983b510dd4d1e6e6bbfd6eeb25b5cb8
oole <ole.c.ostergaard@gmail.com>
2018-09-12 10:48:57 +0200

    switch to test data

--- mil-classification/lymphoma/l_main.py
+++ mil-classification/lymphoma/l_main.py
@@ -23,7 +23,7 @@ def simple_training():
     initialEpoch = 0
 
     netRoot = "/home/oole/lymphoma_net/"
-    runName = "lymphoma_simple_180815/"
+    runName = "lymphoma_simple_180910/"
     modelName = "lymph_model"
 
     if not os.path.exists(netRoot):
@@ -44,34 +44,33 @@ def simple_training():
     # load data
     # split into train val
     dataPath = "/home/oole/data_lymphoma/"
-    trainSlideData = ldata.collect_data(dataPath)
-    valSlideData = data_tf.SlideData(trainSlideData.getSlideList(),
-                                     None, np.asarray(trainSlideData.getSlideList()).size,
-                                     trainSlideData.getSlideLabelList(),
-                                     trainSlideData.getLabelFunc(),
-                                     False,
-                                     labelencoder=trainSlideData.getLabelEncoder(), parseFunctionAugment=trainSlideData.getparseFunctionNormal(), parseFunction=trainSlideData.getparseFunctionNormal())
+    basePath = "/home/oole/data_lymphoma/"
+    trainDataPath = basePath + "train/"
+    testDataPath = basePath + "test/"
+    trainSlideData = ldata.collect_data(trainDataPath)
+    testSlideData = ldata.collect_data(testDataPath)
 
-    train.train_net(trainSlideData, valSlideData,
+    train.train_net(trainSlideData, testSlideData,
                     num_epochs=epochs, batch_size=BATCH_SIZE,
                     savepath = simple_train_savepath, do_augment = True,
                     model_name=modelName, getlabel_train=ldata.getlabel, log_savepath=logreg_savepath,
-                    runName=runName, lr=0.0001,
-                    buildNet = lnet.getLymphNet)
+                    runName=runName, lr=LEARNING_RATE,
+                    buildNet = lnet.getLymphNet, valIsTestData=True)
 
     print("Data collected.")
-
 
 '''
 This will compare the networks performance to the knime blogpost on lymphoma classification.
 
 Dataset must be pre split into training/testing
 '''
-def em_training():
-    initialEpoch = 2
+
+def continue_simple_training():
+    epochs = 100
+    initialEpoch = 100
 
     netRoot = "/home/oole/lymphoma_net/"
-    runName = "lymphoma_em_180815_2/"
+    runName = "lymphoma_simple_180907_cont_sanity/"
     modelName = "lymph_model"
 
     if not os.path.exists(netRoot):
@@ -83,32 +82,79 @@ def em_training():
     else:
         print("Run folder already extists.")
 
-    simple_train_loadpath = netRoot + "lymphoma_simple_180815/" + "lymph_simple"
+    old_simple_savepath = netRoot + "lymphoma_simple_180907/" + "lymph_simple"
+    simple_cont_savepath = netRoot + runName + modelName
+    logfile_path = netRoot + runName + "lymph_net_log.csv"
+    logreg_savepath = netRoot + runName + "lymph_logreg"
+
+    # load data
+    # split into train val
+    basePath = "/home/oole/data_lymphoma/"
+    trainDataPath = basePath + "train/"
+    testDataPath = basePath + "test/"
+    trainSlideData = ldata.collect_data(trainDataPath)
+    testSlideData = ldata.collect_data(testDataPath)
+
+    train.train_net(trainSlideData, testSlideData,
+                    num_epochs=epochs, batch_size=BATCH_SIZE,
+                    savepath=simple_cont_savepath, do_augment=True,
+                    model_name=modelName, getlabel_train=ldata.getlabel, log_savepath=logreg_savepath,
+                    runName=runName, lr=LEARNING_RATE,
+                    buildNet=lnet.getLymphNet,
+                    valIsTestData=True,
+                    initialEpoch=initialEpoch,
+                    loadpath=old_simple_savepath)
+
+    print("Data collected.")
+
+
+'''
+This will compare the networks performance to the knime blogpost on lymphoma classification.
+
+Dataset must be pre split into training/testing
+'''
+def em_training():
+    initialEpoch = 2
+    trainepochs = 200
+
+    netRoot = "/home/oole/lymphoma_net/"
+    runName = "lymphoma_em_180910/"
+    modelName = "lymph_model"
+
+    if not os.path.exists(netRoot):
+        os.makedirs(netRoot)
+    else:
+        print("Net root folder already extists.")
+    if not os.path.exists(netRoot + runName):
+        os.makedirs(netRoot + runName)
+    else:
+        print("Run folder already extists.")
+
+    simple_train_loadpath = netRoot + "lymphoma_simple_180910/" + "lymph_simple"
     em_train_savepath = netRoot + runName + "lympf_em"
     logfile_path = netRoot + runName + "lymph_net_log_em.csv"
     logreg_savepath = netRoot + runName + "lymph_logreg"
 
-
     # load data
     # split into train val
-    dataPath = "/home/oole/data_lymphoma/"
-    trainSlideData = ldata.collect_data(dataPath)
-    valSlideData = data_tf.SlideData(trainSlideData.getSlideList(),
-                                     None, np.asarray(trainSlideData.getSlideList()).size,
-                                     trainSlideData.getSlideLabelList(),
-                                     trainSlideData.getLabelFunc(),
-                                     False,
-                                     labelencoder=trainSlideData.getLabelEncoder(), parseFunctionAugment=trainSlideData.getparseFunctionNormal(), parseFunction=trainSlideData.getparseFunctionNormal())
+    basePath = "/home/oole/data_lymphoma/"
+    trainDataPath = basePath + "train/"
+    testDataPath = basePath + "test/"
+    trainSlideData = ldata.collect_data(trainDataPath)
+    testSlideData = ldata.collect_data(testDataPath)
 
 
-    train_em.emtrain(trainSlideData, valSlideData,
+    train_em.emtrain(trainSlideData, testSlideData,
                      simple_train_loadpath, em_train_savepath, BATCH_SIZE,
                      initial_epochnum=initialEpoch,
                      model_name=modelName,
                      spatial_smoothing=SPATIALSMOOTHING,
                      do_augment=True,
-                     num_epochs=100, dropout_ratio=DROPOUT_RATIO, learning_rate=LEARNING_RATE, sanity_check=False,
-                     logfile_path=logfile_path, logreg_savepath=logreg_savepath, runName=runName, netAcc=None, buildNet = lnet.getLymphNet)
+                     num_epochs=trainepochs, dropout_ratio=DROPOUT_RATIO, learning_rate=LEARNING_RATE, sanity_check=False,
+                     logfile_path=logfile_path, logreg_savepath=logreg_savepath, runName=runName,
+                     netAcc=None,
+                     buildNet = lnet.getLymphNet,
+                     valIsTestData=True)
 
     print("Data collected.")
 
@@ -165,6 +211,8 @@ def continue_em_training():
 
 # simple_training()
 
-# em_training()
+# continue_simple_training()
 
-continue_em_training()
+em_training()
+
+# continue_em_training()
