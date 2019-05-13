b576ce741755612d28ac73577fc1c5eec174df62
oole <ole.c.ostergaard@gmail.com>
2019-05-13 23:27:56 +0200

    Configure cross entropy for full run. (Entropy TH 0.8)

--- mil-classification/vgg_lymphoma/vgg_l_main_xval.py
+++ mil-classification/vgg_lymphoma/vgg_l_main_xval.py
@@ -9,7 +9,6 @@ import data_tf
 from patch_selection.disc_entropy import EctropyDiscFinder
 from disc_patch_select.disc_original import OriginalDiscFinder
 
-
 SPLIT_SEED = 1337
 
 BATCH_SIZE = 256
@@ -20,14 +19,16 @@ THRESHOLD_PERCENTILE = 0.8
 TRAIN_DISC_FINDER = OriginalDiscFinder
 PRED_DISC_FINDER = EctropyDiscFinder(THRESHOLD_PERCENTILE)
 
-
 BASENAME = "vgg_lymph_xval"
-SIMPLERUNSTAMP = "190513_l-0.0001_drop-0.5_bs-256_entropy-" + str(THRESHOLD_PERCENTILE)
+SIMPLERUNSTAMP = "190513_l-" + str(LEARNING_RATE) + "_drop-" + str(DROPOUT_RATIO) + "_bs-" + str(
+    BATCH_SIZE) + "_entropy-" + str(THRESHOLD_PERCENTILE)
 
 '''
 Trains the two-layer model in one go, and performs cross-validation.
 '''
-def cross_val_training(numberOfEpochs = 10):
+
+
+def cross_val_training(numberOfEpochs=150):
     initialEpoch = 0
 
     netRoot = "/home/oole/lymphoma_net_vgg/"
@@ -38,13 +39,9 @@ def cross_val_training(numberOfEpochs = 10):
     else:
         print("Net root folder already extists.")
 
-
-
-
-
     # load data
     # split into train val
-    basePath = "/home/oole/data_lymphoma_slim/"
+    basePath = "/home/oole/data_lymphoma_full/"
     trainDataPath = basePath + "train/"
     # testDataPath = basePath + "test/"
     trainSlideData = ldata.collect_data(trainDataPath)
@@ -56,7 +53,8 @@ def cross_val_training(numberOfEpochs = 10):
 
     foldNum = 1
 
-    for trainSlideData, testSLideData in data_tf.KFoldSlideList(trainSlideData, testSlideData, numberOfSplits=5, splitSeed=1337):
+    for trainSlideData, testSLideData in data_tf.KFoldSlideList(trainSlideData, testSlideData, numberOfSplits=5,
+                                                                splitSeed=1337):
         print("Fold number: " + str(1))
         # train slide data should consist of all available training data
         # is split into test and training
@@ -74,32 +72,32 @@ def cross_val_training(numberOfEpochs = 10):
         if netAcc is not None:
             netAcc.getSummmaryWriter(runName, sess.graph, forceNew=True)
 
-
         _, _, netAcc = train.train_net(trainSlideData, testSlideData,
-                            num_epochs=2,
-                            batch_size=BATCH_SIZE,
-                            savepath = train_savepath,
-                            do_augment = True,
-                            model_name=modelName,
-                            getlabel_train=ldata.getlabel,
-                            log_savepath=logreg_savepath,
-                            runName=runName,
-                            lr=LEARNING_RATE,
-                            buildNet = lnet.getLymphNet,
-                            valIsTestData=True,
-                            splitSeed=SPLIT_SEED,
-                            sess = sess,
-                            netAcc = netAcc)
+                                       num_epochs=2,
+                                       batch_size=BATCH_SIZE,
+                                       savepath=train_savepath,
+                                       do_augment=True,
+                                       model_name=modelName,
+                                       getlabel_train=ldata.getlabel,
+                                       log_savepath=logreg_savepath,
+                                       runName=runName,
+                                       lr=LEARNING_RATE,
+                                       buildNet=lnet.getLymphNet,
+                                       valIsTestData=True,
+                                       splitSeed=SPLIT_SEED,
+                                       sess=sess,
+                                       netAcc=netAcc)
 
         print("Finished Simple Training")
 
         train_em.emtrain(trainSlideData, testSlideData,
                          None, train_savepath, BATCH_SIZE,
-                         initial_epochnum=initialEpoch+2,
+                         initial_epochnum=initialEpoch + 2,
                          model_name=modelName,
                          spatial_smoothing=SPATIALSMOOTHING,
                          do_augment=True,
-                         num_epochs=initialEpoch+numberOfEpochs, dropout_ratio=DROPOUT_RATIO, learning_rate=LEARNING_RATE, sanity_check=False,
+                         num_epochs=initialEpoch + numberOfEpochs, dropout_ratio=DROPOUT_RATIO,
+                         learning_rate=LEARNING_RATE, sanity_check=False,
                          logfile_path=logfile_path,
                          logreg_savepath=logreg_savepath,
                          runName=runName,
@@ -107,13 +105,12 @@ def cross_val_training(numberOfEpochs = 10):
                          valIsTestData=True,
                          discriminativePatchFinderTrain=TRAIN_DISC_FINDER,
                          discriminativePatchFinderPredict=PRED_DISC_FINDER,
-                         splitSeed = SPLIT_SEED,
-                         sess= sess)
+                         splitSeed=SPLIT_SEED,
+                         sess=sess)
         foldNum += 1
 
         # Reset weights for each iteration
         sess.run(tf.global_variables_initializer())
-
 
 
 print("Start KFold Crossval")
